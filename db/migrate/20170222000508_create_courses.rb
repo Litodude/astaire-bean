591f78197a79ac0ecaf6080f7cc14d2a432f3255
Lito <litodude@ymail.com>
2017-02-22 22:31:26 +0000

    changed start_time to datetime

--- db/migrate/20170222000508_create_courses.rb
+++ db/migrate/20170222000508_create_courses.rb
@@ -2,7 +2,7 @@ class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
       t.string   "class_name"
-      t.string "start_time"
+      t.datetime "start_time"
       t.integer "duration"
       t.timestamps null: false
 

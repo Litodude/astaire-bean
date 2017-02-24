82bfdd06ec5fea3c16c60ac0f7c6c5c7c709cbab
Lito <lito@litodude.com>
2017-02-23 21:32:05 -0800

    user controller default actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,56 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def edit
+		@user = User.fine(params[:id])
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			message = 'Success! Your account has been created.'
+		else
+			message = 'Please correct these errors: #{@user.errors.full)messages.join(', ')}'
+			flash.now[:error] = message
+			render 'users/new'
+		end
+	end
+
+	def update
+		@user = User.find(paramds([:id])
+		@user.assign_attributes(teacher_params)
+
+		if @user.save
+			message = 'Success! Your changes have been saved.'
+			redirect to edit_user_url(@user.id), flash => {:success =>message}
+		else
+			message = 'Please correct these errors: #{@user.errors.full_message.join(', ')}'
+			flash.now[:error] = message
+			render 'users/edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+
+		respond_to do |f|
+			f.html { redirect_tp users_url }
+			f.json { render :json => {} }
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:full_name, :email, :birth_date, :phone_number)
+	end
+	
 end
